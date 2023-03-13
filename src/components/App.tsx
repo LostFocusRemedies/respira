6b0b73cda83448b28529bbf1de25025756b4d483
Massimiliano Truzzi <massimiliano.truzzi@gmail.com>
2023-03-13 13:09:07 +0100

    bulma setup, and components folder structure

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import './counter/Counter';
+
+function App() {
+  return (
+    <div className="container">
+      <p className="title">RESPIRA</p>
+      <p className="subtitle">Some Componet will go down below here. </p>
+    </div>
+  );
+}
+
+export default App;
